ce6f28bac29d2094bc81d724750d49a269a65be0
nicoflorentin <nicoflorentn@gmail.com>
2023-10-20 04:40:34 -0300

    add vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: './src/assets/icons/icons.jsx',
+      external: "/src/assets/icons/icons.jsx",
     },
   },
 })
