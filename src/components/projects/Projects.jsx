d9b81672d0051ae5a4df46734394c74e5ca81486
nicoflorentin <nicogrunger@hotmail.com>
2024-01-29 21:15:10 -0300

    style: projects overflow fix

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -26,39 +26,55 @@ const projects = [
 		githubURL: "http://example.com",
 		deployURL: "http://example.com",
 	},
-	{
-		id: 4,
-		title: "Cuarto Proyecto",
-		description: "Brief description of the project, explaining processes and functionalities",
-		image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmedium.com%2Fcodex%2Fthe-programming-language-s-of-the-future-24af5cfa8479&psig=AOvVaw0iDjnBn5niSF20Lv1GcQU9&ust=1706651682379000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCLiHzYzLg4QDFQAAAAAdAAAAABAE",
-		githubURL: "http://example.com",
-		deployURL: "http://example.com",
-	},
+	// {
+	// 	id: 4,
+	// 	title: "Cuarto Proyecto",
+	// 	description: "Brief description of the project, explaining processes and functionalities",
+	// 	image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmedium.com%2Fcodex%2Fthe-programming-language-s-of-the-future-24af5cfa8479&psig=AOvVaw0iDjnBn5niSF20Lv1GcQU9&ust=1706651682379000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCLiHzYzLg4QDFQAAAAAdAAAAABAE",
+	// 	githubURL: "http://example.com",
+	// 	deployURL: "http://example.com",
+	// },
+	// {
+	// 	id: 5,
+	// 	title: "Quinto Proyecto",
+	// 	description: "Brief description of the project, explaining processes and functionalities",
+	// 	image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmedium.com%2Fcodex%2Fthe-programming-language-s-of-the-future-24af5cfa8479&psig=AOvVaw0iDjnBn5niSF20Lv1GcQU9&ust=1706651682379000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCLiHzYzLg4QDFQAAAAAdAAAAABAE",
+	// 	githubURL: "http://example.com",
+	// 	deployURL: "http://example.com",
+	// },
+	// {
+	// 	id: 6,
+	// 	title: "Sexto Proyecto",
+	// 	description: "Brief description of the project, explaining processes and functionalities",
+	// 	image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmedium.com%2Fcodex%2Fthe-programming-language-s-of-the-future-24af5cfa8479&psig=AOvVaw0iDjnBn5niSF20Lv1GcQU9&ust=1706651682379000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCLiHzYzLg4QDFQAAAAAdAAAAABAE",
+	// 	githubURL: "http://example.com",
+	// 	deployURL: "http://example.com",
+	// },
 ]
 
 const Projects = () => {
-	const ProjectsMap = () => {
-		return projects.map(project => <Project key={project.id} {...project} />)
-	}
+  const ProjectsMap = () => {
+    return projects.map(project => <Project key={project.id} {...project} />);
+  };
 
-	return (
-		<section className="h-[100vh]">
-			<h2
-				className="text-primary text-3xl font-bold
-								text-center
-								p-10
-								"
-			>
-				WORK
-			</h2>
-			<div
-				className="flex flex-col gap-8
-									font-consolas"
-			>
-				<ProjectsMap />
-			</div>
-		</section>
-	)
-}
+  return (
+    <section className="h-[100vh] ">
+      <h2
+        className="text-primary text-3xl font-bold
+                   text-center
+                   p-10"
+      >
+        WORK
+      </h2>
+      <div
+        className="flex flex-col gap-8
+                   font-consolas
+									 h-[80vh] overflow-y-auto" // Añade esta línea para permitir el desplazamiento vertical
+      >
+        <ProjectsMap />
+      </div>
+    </section>
+  );
+};
 
-export default Projects
+export default Projects;
