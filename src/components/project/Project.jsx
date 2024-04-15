28041f4f21bbcb703eb4de2b79f7aae5df446625
nicoflorentin <nicogrunger@hotmail.com>
2024-04-15 16:47:06 -0300

    styles : change flex in projects component - add border to project image

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -4,13 +4,13 @@ import config from "../../../tailwind.config";
 
 const Project = ({ title, description, image, githubURL, deployURL }) => {
 	return (
-		<div className="flex flex-col gap-3 text-center px-5 my-3 m-auto max-w-xs border-r-2 border-bone md:max-w-full md:flex-row items-center">
+		<div className="flex flex-col gap-3 text-center px-5 my-3 m-auto max-w-xs border-r-2 border-bone md:gap-10 md:max-w-full md:flex-row items-center">
 			<div className="md:order-2 self-center">
 				<h3 className="text-bone font-medium text-lg uppercase tracking-widest mb-5 md:mb-2 md:order-2">{title}</h3>
 				<p className="text-bone font-light leading-5 text-sm tracking-wide">{description}</p>
 			</div>
-			<img src={image} alt="projectImage" className="object-cover h-36 w-44 rounded-sm md:rounded-none md:order-1" />
-			<div className="flex justify-center items-center gap-6 mt-1 font-semibold md:order-2">
+			<img src={image} alt="projectImage" className="object-cover border-2 border-secondary h-36 w-44 rounded-sm md:rounded-none md:order-1" />
+			<div className="flex md:flex-col justify-center items-center gap-6 mt-1 font-semibold md:order-2">
 				<a href={githubURL} className="text-secondary text-sm px-3 border-l-2 border-secondary hover:text-primary hover:border-primary transition-all duration-fast">
 					REPO
 				</a>
