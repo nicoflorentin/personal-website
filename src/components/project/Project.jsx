5091ab74b28a2a9659102541a14027f7e6fe41cb
nicoflorentin <nicogrunger@hotmail.com>
2024-09-18 14:34:26 -0300

    feat : update projects

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -7,14 +7,16 @@ const Project = ({ title, description, image, githubURL, deployURL }) => {
 				<h3 className="text-bone font-medium text-lg uppercase tracking-widest mb-5 md:mb-2 md:order-2">{title}</h3>
 				<p className="text-bone font-light leading-5 text-sm tracking-wide">{description}</p>
 			</div>
-			<img src={image} alt="projectImage" className="object-cover border-l-2 border-secondary h-36 w-44 rounded-sm md:rounded-none md:order-1" />
+			<div className="h-40 min-w-[150px] max-w-[300px]">
+				<img src={image} alt="projectImage" className="object-cover w-full h-full border-l-2 pl-2 border-secondary rounded-sm md:rounded-none md:order-1" />
+			</div>
 			<div className="flex md:flex-col justify-center items-center gap-6 mt-1 font-semibold md:order-2">
 				<a href={githubURL} className="text-secondary text-sm px-3 border-l-2 border-secondary hover:text-primary hover:border-primary transition-all duration-fast">
 					REPO
 				</a>
-				<a href={deployURL} className="text-secondary text-sm px-3 border-l-2 border-secondary hover:text-primary hover:border-primary transition-all duration-fast">
+				{deployURL && <a href={deployURL} className="text-secondary text-sm px-3 border-l-2 border-secondary hover:text-primary hover:border-primary transition-all duration-fast">
 					VISIT
-				</a>
+				</a>}
 			</div>
 		</div>
 	);
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,50 +1,61 @@
 export const projects = [
-  {
-    id: "automenu",
-    title: "AutoMenu",
-    description: "Instantly generate modern design menus and manage every detail from a feature-rich dashboard. Discover an efficient and powerful way to take your restaurant to the next level",
-    image: "https://github.com/nicoflorentin/auto-menu/assets/81888574/56c68267-7bad-4d86-b98e-cacf9bd3a7cb",
-    githubURL: "https://github.com/nicoflorentin/auto-menu",
-    deployURL: "https://auto-menu-app.vercel.app/"
-  },
-  {
-    id: "tokio",
-    title: "Tokio Blues Dialog Simulator",
-    description: "Immerse yourself in the story through interactive dialogues that allow you to choose different responses and see how they influence the narrative. Based on the novel 'Norwegian Wood' (Tokyo Blues) by Haruki Murakami.",
-    image: "https://github.com/nicoflorentin/dialog-simulator/raw/master/dialog-simulator-gif-demo.gif",
-    githubURL: "https://github.com/nicoflorentin/dialog-simulator",
-    deployURL: "https://tokiobluesdialogs.netlify.app/"
-  },
-  {
-    id: "rickandmorty",
-    title: "RyM Viewer",
-    description: "This project queries the Rick and Morty API to display the characters information. It allows users to implement filters and search by text with an unique and charming design",
-    image: "https://github.com/nicoflorentin/rick_and_morty/assets/81888574/507eedd3-e0dc-46c8-9329-7b4dcc0465c8",
-    githubURL: "https://github.com/nicoflorentin/rick_and_morty",
-    deployURL: "https://rymviewer.netlify.app"
-  },
-  {
-    id: "nature",
-    title: "NatureXtreme",
-    description: "Ecommerce with full CRUD functionality, login and a complete dashboard view",
-    image: "https://github.com/nicoflorentin/portfolio/assets/81888574/eb497ade-2788-4c2a-b738-1f26ae5d16fc",
-    githubURL: "https://github.com/nicoflorentin/NaturalezaXtreme",
-    deployURL: "https://naturaleza-xtreme.vercel.app/"
-  },
-  // {
-  //   id: "matrix",
-  //   title: "Matrix Navigator",
-  //   description: "From a list of information without coordinates, this application assigns a location to each element and allows coherent navigation to the chosen direction.",
-  //   image: "https://github.com/nicoflorentin/matrix-navigator/assets/81888574/4d34d1be-d91b-4ca7-93f7-f92410c23cb1",
-  //   githubURL: "https://github.com/nicoflorentin/matrix-navigator",
-  //   deployURL: "https://matrix-navigator.vercel.app/"
-  // },
-  // {
-  //   id: "pokemon",
-  //   title: "Pokémon Viewer",
-  //   description: "This project queries the Pokémon API to display the creatures information. It allows users to implement filters and search by text. The cherry on top is the posibility to create your own Pokémon.",
-  //   image: "https://github.com/nicoflorentin/pokemon-viewer/assets/81888574/4f50c15b-fe39-4dd4-add9-cb3663d368f4",
-  //   githubURL: "https://github.com/nicoflorentin/pokemon-viewer",
-  //   deployURL: "https://pokemon-viewer-pi.vercel.app/"
-  // }
-];
+	{
+		id: "automenu",
+		title: "AutoMenu",
+		description:
+			"Instantly generate modern design menus and manage every detail from a feature-rich dashboard. Discover an efficient and powerful way to take your restaurant to the next level",
+		image: "https://github.com/nicoflorentin/auto-menu/assets/81888574/56c68267-7bad-4d86-b98e-cacf9bd3a7cb",
+		githubURL: "https://github.com/nicoflorentin/auto-menu",
+		deployURL: "https://auto-menu-app.vercel.app/",
+	},
+	{
+		id: "rn-pomodoro-day-planner",
+		title: "React Native Pomodoro Day Planner",
+		description:
+			"Organize your day with a focus on productivity using the Pomodoro technique. Plan tasks, track progress, and boost your efficiency with this intuitive mobile app.",
+		image: "https://github.com/user-attachments/assets/c1611870-7fe7-4996-9bd3-20d090751c43",
+		githubURL: "https://github.com/nicoflorentin/RN-pomodoro-day-planner",
+		deployURL: "",
+	},
+	{
+		id: "tokio",
+		title: "Tokio Blues Dialog Simulator",
+		description:
+			"Interactive dialogues app that allow you to choose different responses and see how they influence the narrative. A hobby project I thoroughly enjoyed creating, based on the novel 'Norwegian Wood' (Tokyo Blues) by Haruki Murakami",
+		image: "https://github.com/nicoflorentin/dialog-simulator/raw/master/dialog-simulator-gif-demo.gif",
+		githubURL: "https://github.com/nicoflorentin/dialog-simulator",
+		deployURL: "https://tokiobluesdialogs.netlify.app/",
+	},
+	// {
+	//   id: "rickandmorty",
+	//   title: "RyM Viewer",
+	//   description: "This project queries the Rick and Morty API to display the characters information. It allows users to implement filters and search by text with an unique and charming design",
+	//   image: "https://github.com/nicoflorentin/rick_and_morty/assets/81888574/507eedd3-e0dc-46c8-9329-7b4dcc0465c8",
+	//   githubURL: "https://github.com/nicoflorentin/rick_and_morty",
+	//   deployURL: "https://rymviewer.netlify.app"
+	// },
+	{
+		id: "nature",
+		title: "NatureXtreme",
+		description: "Ecommerce with full CRUD functionality, login and a complete dashboard view",
+		image: "https://github.com/nicoflorentin/portfolio/assets/81888574/eb497ade-2788-4c2a-b738-1f26ae5d16fc",
+		githubURL: "https://github.com/nicoflorentin/NaturalezaXtreme",
+		deployURL: "https://naturaleza-xtreme.vercel.app/",
+	},
+	// {
+	//   id: "matrix",
+	//   title: "Matrix Navigator",
+	//   description: "From a list of information without coordinates, this application assigns a location to each element and allows coherent navigation to the chosen direction.",
+	//   image: "https://github.com/nicoflorentin/matrix-navigator/assets/81888574/4d34d1be-d91b-4ca7-93f7-f92410c23cb1",
+	//   githubURL: "https://github.com/nicoflorentin/matrix-navigator",
+	//   deployURL: "https://matrix-navigator.vercel.app/"
+	// },
+	// {
+	//   id: "pokemon",
+	//   title: "Pokémon Viewer",
+	//   description: "This project queries the Pokémon API to display the creatures information. It allows users to implement filters and search by text. The cherry on top is the posibility to create your own Pokémon.",
+	//   image: "https://github.com/nicoflorentin/pokemon-viewer/assets/81888574/4f50c15b-fe39-4dd4-add9-cb3663d368f4",
+	//   githubURL: "https://github.com/nicoflorentin/pokemon-viewer",
+	//   deployURL: "https://pokemon-viewer-pi.vercel.app/"
+	// }
+]
