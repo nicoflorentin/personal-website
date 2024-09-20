baf82b029fd55edc1eef087474849f9bc9f91729
nicoflorentin <nicogrunger@hotmail.com>
2024-09-20 01:02:52 -0300

    feat : download resume button

--- src/components/main/CvDownloadItem.jsx
+++ src/components/main/CvDownloadItem.jsx
@@ -20,7 +20,7 @@ const CvDownloadItem = () => {
 
 	return (
 		<div>
-			{!showConfirm ? <li onClick={toggleDownloadVisibility}>RESUME</li> : <li>
+			{!showConfirm ? <li className='hover:text-primary' onClick={toggleDownloadVisibility}>RESUME</li> : <li>
 				<span className='hover:text-primary'><a href={resume} download={'NicolasFlorentin.pdf'}>DOWNLOAD RESUME</a><span className='absolute ml-1'>?</span></span>
 				<span className='absolute pl-2 ml-4 hover:text-red-400 hover:scale-[1.1] text-secondary' onClick={closeDownloadConfirmHandler}>✖</span>
 			</li>}
