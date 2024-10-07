41009f836e11dce9c5ce6885faabb624396c2d37
nicoflorentin <nicogrunger@hotmail.com>
2024-10-07 18:30:09 -0300

    fix : non animated icons doesnt render during animation period

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -6,11 +6,9 @@ import CvDownloadItem from "./CvDownloadItem";
 import { MAIN_ICONS_ABOUT, MAIN_ICONS_PROJECTS, MAIN_ICONS_RESUME } from "../../constants/constants";
 import NameLabel from "./NameLabel";
 import Header from "./Header";
-import { useRef } from "react";
 
 const Main = ({ scrollToSection }) => {
 	const [hoveredItem, setHoveredItem] = useState(null);
-	const hovereableItem = useRef(true)
 	const [clickedItem, setClickedItem] = useState(null);
 
 	const LIST_ITEM_ICON_SIZE = 20;
@@ -38,16 +36,18 @@ const Main = ({ scrollToSection }) => {
 
 	const handleClick = (item) => {
 		setClickedItem(item.name);
-		hovereableItem.current = false
 		setTimeout(() => {
-			setClickedItem(null)
 			item.onClick()
 		}, 500);
 		setTimeout(() => {
+			setClickedItem(null)
 			setHoveredItem(null)
-			hovereableItem.current = true
-		},1000)
+		}, 1000)
 	};
+
+	const handleHover = (itemName) => {
+		setHoveredItem(itemName)
+	}
 
 	return (
 		<section className="h-[100vh] m-auto flex flex-col justify-between max-w-2xl sm:max-w-full">
@@ -56,24 +56,24 @@ const Main = ({ scrollToSection }) => {
 				<div className="flex flex-col items-center lg:flex-row lg:justify-center lg:gap-52 xl:gap-[500px]">
 					<NameLabel />
 					<nav className="navbar">
-						<ul className="flex flex-col gap-2 w-52 font-inter text-bone text-sm tracking-[2px] font-[700] lg:text-end lg:items-end">
+						<ul className="flex flex-col gap-2 w-52 font-inter text-bone text-sm tracking-[2px] font-[700] overflow-hidden lg:text-end lg:items-end">
 							{navConfig.map((itemConfig, index) => (
 								<li
 									className="flex items-center transition-all duration-fast cursor-pointer w-fit justify-end hover:text-primary"
 									key={index}
-									onMouseEnter={() => setHoveredItem(itemConfig.name)}
+									onMouseEnter={() => handleHover(itemConfig.name)}
 									onMouseLeave={() => setHoveredItem(null)}
 									onClick={() => handleClick(itemConfig)}
 								>
-									<span>{itemConfig.content} </span>
+									<span>{itemConfig.content}</span>
 									{hoveredItem === itemConfig.name && (
 										<motion.span
 											className="absolute"
 											initial={{ x: 35, opacity: 0 }}
-											animate={clickedItem === itemConfig.name ? { x: 500, opacity: 0 } : { x: 25, opacity: 1 }}
+											animate={clickedItem === itemConfig.name ? { x: 100, opacity: 0 } : { x: 30, opacity: 1 }}
 											transition={{ duration: 0.3, ease: "easeOut" }}
 										>
-											{itemConfig.icon}
+											{clickedItem ? clickedItem === itemConfig.name ? itemConfig.icon : null : itemConfig.icon}
 										</motion.span>
 									)}
 								</li>
