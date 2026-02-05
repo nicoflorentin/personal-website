09e737e9987998db5742abde82d7ce127949341d
Nicolás Florentín <81888574+nicoflorentin@users.noreply.github.com>
2026-02-05 14:23:48 -0300

    feat / change projects content

--- src/components/project/ProjectTag.jsx
+++ src/components/project/ProjectTag.jsx
@@ -1,6 +1,6 @@
 const ProjectTag = ({ label }) => {
 	return (
-		<div className="text-secondary text-xs font-semibold bg-secondary/20 px-2 py-1 rounded-full">{label}</div>
+		<div className="text-secondary text-xs lg:text-sm font-semibold xl:font-medium bg-secondary/20 px-2 py-1 rounded-full">{label}</div>
 	)
 }
 
