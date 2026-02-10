bbfe92f24f32d8a9c5c191cdb0979ab8e85c6ae8
Nicolás Florentín <81888574+nicoflorentin@users.noreply.github.com>
2026-02-10 15:36:50 -0300

    styles / change namelabel fontsize

--- src/components/main/NameLabel.jsx
+++ src/components/main/NameLabel.jsx
@@ -17,9 +17,9 @@ const NameLabel = () => {
 		}
 	}, [])
 	return (
-		<div className="text-primary">
+		<div className="text-primary max-[370px]:pl-2">
 			<h1 className="text-[16px] tracking-wide pl-1 sm:pl-2 font-inter">NICOLÁS FLORENTÍN</h1>
-			<h2 className="font-rubik leading-[0.95em] md:tracking-[5px] mb-20 text-[80px]">
+			<h2 className="font-rubik leading-[0.95em] md:tracking-[5px] mb-20 text-[80px] max-[370px]:text-[70px]">
 				FRONT<br className='md:hidden' />END <br /> DEV{barIsVisible ? '' : '/'}
 			</h2>
 			<p className="max-w-xs text-[12px] font-medium tracking-[2px] opacity-70 font-inter uppercase sm:mt-[-70px] mt-[-80px] mb-20">
